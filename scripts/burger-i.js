c5a3af93dafe4cf350c84571308d9b2390a87241
flarepikachu <91821771+flarepikachu@users.noreply.github.com>
2022-10-05 17:38:43 +0800

    fixburger

--- scripts/burger-i.js
+++ scripts/burger-i.js
@@ -1,23 +1,21 @@
 class burgerinsert extends HTMLElement {
-    constructor() {
-      super();
-    }
-  
-    connectedCallback() {
-      this.innerHTML = `
-      <header>
-        <div class="headerdiv">
-            <span>
-                <a class='site-title' href="#"><span>Flairy Domain</span></a>
-            </span>
-        </div>
-    </header>
-      `;
-    }
+  constructor() {
+    super();
   }
-  
-  customElements.define('burger-i', burgerinsert);
+
+  connectedCallback() {
+    this.innerHTML = `
+      <div class="mobile-burger">
+        <a href="javascript:void(0);" class="burger" onclick="navigationbar()">
+            <i class="fa-solid fa-bars fa-2xl">
+            </i>
+        </a>
+    </div>
+      `;
+  }
+}
+
+customElements.define('burger-i', burgerinsert);
 
 
 
-  

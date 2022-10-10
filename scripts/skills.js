c723934d91163a57b87c7b6294a45b13726a83ad
pikachuflare <mc.pikachuflare@gmail.com>
2022-10-10 16:03:34 +0800

    commit ss

--- scripts/skills.js
+++ scripts/skills.js
@@ -4,140 +4,90 @@ function popup1() {
   var x = document.getElementById("popup-1");
   var dimple = document.getElementById("dim");
   var closer = document.getElementById("closer")
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    dimple.classList.toggle('dimmer-active')
-  } else {
-    x.style.display = "block";
-    closer.style.display = "block";
-    dimple.classList.toggle('dimmer-active')
-  }
+  x.style.display = "block";
+  closer.style.display = "block";
+  dimple.classList.toggle('dimmer-active')
 }
 
 function popup1m() {
   var x = document.getElementById("popup-1-m");
   var dimple = document.getElementById("dim");
   var closer = document.getElementById("closer")
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    dimple.classList.toggle('dimmer-active')
-  } else {
-    x.style.display = "block";
-    closer.style.display = "block";
-    dimple.classList.toggle('dimmer-active')
-  }
+  x.style.display = "block";
+  closer.style.display = "block";
+  dimple.classList.toggle('dimmer-active')
 }
 
 function popup2() {
   var x = document.getElementById("popup-2");
   var dimple = document.getElementById("dim");
   var closer = document.getElementById("closer")
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    dimple.classList.toggle('dimmer-active')
-  } else {
-    x.style.display = "block";
-    closer.style.display = "block";
-    dimple.classList.toggle('dimmer-active')
-  }
+  x.style.display = "block";
+  closer.style.display = "block";
+  dimple.classList.toggle('dimmer-active')
 }
 
 function popup2m() {
   var x = document.getElementById("popup-2-m");
   var dimple = document.getElementById("dim");
   var closer = document.getElementById("closer")
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    dimple.classList.toggle('dimmer-active')
-  } else {
-    x.style.display = "block";
-    closer.style.display = "block";
-    dimple.classList.toggle('dimmer-active')
-  }
+  x.style.display = "block";
+  closer.style.display = "block";
+  dimple.classList.toggle('dimmer-active')
 }
 
 function popup3() {
   var x = document.getElementById("popup-3");
   var dimple = document.getElementById("dim");
   var closer = document.getElementById("closer")
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    dimple.classList.toggle('dimmer-active')
-  } else {
-    x.style.display = "block";
-    closer.style.display = "block";
-    dimple.classList.toggle('dimmer-active')
-  }
+  x.style.display = "block";
+  closer.style.display = "block";
+  dimple.classList.toggle('dimmer-active')
 }
 
 function popup3m() {
   var x = document.getElementById("popup-3-m");
   var dimple = document.getElementById("dim");
   var closer = document.getElementById("closer")
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    dimple.classList.toggle('dimmer-active')
-  } else {
-    x.style.display = "block";
-    closer.style.display = "block";
-    dimple.classList.toggle('dimmer-active')
-  }
+  x.style.display = "block";
+  closer.style.display = "block";
+  dimple.classList.toggle('dimmer-active')
 }
 
 function popup4() {
   var x = document.getElementById("popup-4");
   var dimple = document.getElementById("dim");
   var closer = document.getElementById("closer")
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    dimple.classList.toggle('dimmer-active')
-  } else {
-    x.style.display = "block";
-    closer.style.display = "block";
-    dimple.classList.toggle('dimmer-active')
-  }
+  x.style.display = "block";
+  closer.style.display = "block";
+  dimple.classList.toggle('dimmer-active')
 }
 
 function popup4m() {
   var x = document.getElementById("popup-4-m");
   var dimple = document.getElementById("dim");
   var closer = document.getElementById("closer")
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    dimple.classList.toggle('dimmer-active')
-  } else {
-    x.style.display = "block";
-    closer.style.display = "block";
-    dimple.classList.toggle('dimmer-active')
-  }
+  x.style.display = "block";
+  closer.style.display = "block";
+  dimple.classList.toggle('dimmer-active')
 }
 
 function popup5() {
   var x = document.getElementById("popup-5");
   var dimple = document.getElementById("dim");
   var closer = document.getElementById("closer")
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    dimple.classList.toggle('dimmer-active')
-  } else {
-    x.style.display = "block";
-    closer.style.display = "block";
-    dimple.classList.toggle('dimmer-active')
-  }
+  x.style.display = "block";
+  closer.style.display = "block";
+  dimple.classList.toggle('dimmer-active')
 }
 
 function popup5m() {
   var x = document.getElementById("popup-5-m");
   var dimple = document.getElementById("dim");
   var closer = document.getElementById("closer")
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    dimple.classList.toggle('dimmer-active')
-  } else {
-    x.style.display = "block";
-    closer.style.display = "block";
-    dimple.classList.toggle('dimmer-active')
-  }
+  x.style.display = "block";
+  closer.style.display = "block";
+  dimple.classList.toggle('dimmer-active')
 }
 
 

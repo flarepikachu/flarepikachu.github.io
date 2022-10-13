1c38808f7919d7c4be1aac447995200c743f980a
pikachuflare <mc.pikachuflare@gmail.com>
2022-10-13 17:54:51 +0800

    navbar 2

--- scripts/header.js
+++ scripts/header.js
@@ -7,9 +7,15 @@ class Header extends HTMLElement {
       this.innerHTML = `
       <header>
         <div class="headerdiv">
-            <span>
-                <a class='site-title' href="#"><span>Flairy Domain</span></a>
-            </span>
+          <span>
+              <a class='site-title' href="#"><span>Flairy Domain</span></a>
+          </span>
+          <div class="mobile-burger">
+              <a href="javascript:void(0);" class="burger" onclick="navigationbar()">
+                  <i class="fa-solid fa-bars fa-2xl">
+                  </i>
+              </a>
+          </div>
         </div>
     </header>
       `;

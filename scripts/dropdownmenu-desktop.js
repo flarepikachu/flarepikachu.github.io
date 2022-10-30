93df877e2db0304542c566f2a124163419a73aea
pikachuflare <mc.pikachuflare@gmail.com>
2022-10-30 05:59:36 -0400

    1.0.6 Patch Notes

--- scripts/dropdownmenu-desktop.js
+++ scripts/dropdownmenu-desktop.js
@@ -13,12 +13,13 @@ class dropdown1 extends HTMLElement {
               Patch Notes
               </button>
               <div class="dropdown-container dropdown-container-patch-notes">
-              <a class='test' href="/patch-notes/illusion-vanguard.html"><span>Illusion Vanguard</span></a>
-              <a class='test' href="/patch-notes/illusion-vanguard-1.html"><span>Illusion Vanguard Hotfix - 1.0.1</span></a>
-              <a class='test' href="/patch-notes/illusion-vanguard-2.html"><span>Illusion Vanguard Hotfix - 1.0.2</span></a>
-              <a class='test' href="/patch-notes/illusion-vanguard-3.html"><span>Illusion Vanguard Hotfix - 1.0.3</span></a>
-              <a class='test' href="/patch-notes/illusion-vanguard-4.html"><span>Illusion Vanguard Hotfix - 1.0.4</span></a>
+              <a class='test' href="/patch-notes/illusion-vanguard-6.html"><span>Illusion Vanguard Hotfix - 1.0.6</span></a>
               <a class='test' href="/patch-notes/illusion-vanguard-5.html"><span>Illusion Vanguard Hotfix - 1.0.5</span></a>
+              <a class='test' href="/patch-notes/illusion-vanguard-4.html"><span>Illusion Vanguard Hotfix - 1.0.4</span></a>
+              <a class='test' href="/patch-notes/illusion-vanguard-3.html"><span>Illusion Vanguard Hotfix - 1.0.3</span></a>
+              <a class='test' href="/patch-notes/illusion-vanguard-2.html"><span>Illusion Vanguard Hotfix - 1.0.2</span></a>
+              <a class='test' href="/patch-notes/illusion-vanguard-1.html"><span>Illusion Vanguard Hotfix - 1.0.1</span></a>
+              <a class='test' href="/patch-notes/illusion-vanguard.html"><span>Illusion Vanguard</span></a>
               </div>
           </li>
           <li><a class='direlink' href="/index.html">
